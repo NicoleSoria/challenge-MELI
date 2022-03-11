78a278275341312f67c2e0bf1feeb3456a037671
NicoleSoria <tamarasoria35@gmail.com>
2022-03-11 12:46:37 -0300

    puerto de app

--- app.js
+++ app.js
@@ -5,9 +5,10 @@ const items_routes = require('./routes/items.routes');
 env(__dirname + '/.env');
 
 var app = express();
+const PORT = process.env.PORT || '3000' 
 app.use(express.json());
 app.use('/api', items_routes);
 
-app.listen(process.env.PORT, () => {
+app.listen(PORT, () => {
   console.log('Express server listo!!');
 })
