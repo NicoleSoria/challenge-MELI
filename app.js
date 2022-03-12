9a00c56d8d7794e0b288f012d203f41be4ae8a2e
Tamara Soria <tamarasoria35@outlook.com.ar>
2022-03-11 21:07:07 -0300

    corrijo config

--- app.js
+++ app.js
@@ -3,7 +3,9 @@ var cors = require('cors');
 var env = require('node-env-file'); // .env file
 const items_routes = require('./routes/items.routes');
 
-// env(__dirname + '/.env');
+//Habilitar solo para pruebas locales, ya que para probar local usa las variables contenidas en .env
+//env(__dirname + '/.env'); 
+
 const port = process.env.PORT || 3000
 var app = express();
 
